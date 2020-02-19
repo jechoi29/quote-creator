d7e72bbbd732bc7594a4ae0bd2757d27fbc741e7
Jungeun Choi <jechoi29@uw.edu>
2020-02-19 10:54:51 -0800

    Edit main page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from "react";
 import "./App.css";
+import "./media.css";
+import { Button } from "antd";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">
+        <div className="intro">
+          <div className="quotation">“</div>
+          <div className="app-title">Quote Creator</div>
+          <div className="app-description">
+            Create an inspirational quote with a single click
+          </div>
+          <div className="button-wrap">
+            <Button type="primary">Click me!</Button>
+          </div>
+        </div>
+      </header>
     </div>
   );
 }
